d5c45bd5a4e0495d59e652c501c7c92603aa49b2
MXM553341082 <553341082@qq.com>
2023-03-09 01:57:22 +0800

    init

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import type { PluginOption } from 'vite'
+
+const VitePluginVitePress = (): PluginOption => {
+  return {
+    name: 'vite-plugin-VitePress',
+  }
+}
+
+export { VitePluginVitePress }
+export default VitePluginVitePress
