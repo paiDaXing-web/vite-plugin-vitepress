cf25658754f09192b8243d2ea2e81e59294ed752
MXM553341082 <553341082@qq.com>
2023-03-13 22:42:29 +0800

    sss

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Inspect from "vite-plugin-inspect"
+import VitePluginVitePress from '../src'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [VitePluginVitePress(),vue(),Inspect()],
 })
